2945c0bf61700d7d04d67d5fe0ec0885512ab8cc
Vivek Rai <96563624+vivekrai1999@users.noreply.github.com>
2022-02-25 22:28:32 +0530

    Create hello-world.cpp

---
+++ hello-world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+  cout<<"Hello World"<<endl;
+}
+
+// endl is used to enter to the next line, another alternative is using \n
+// every cpp program starts from the main 
+// iostream is the library which contains all code for cout
